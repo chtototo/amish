8319e61f9dec35d26f3c58ea86a9ce4aa37ab4a7
Amina Magomedova <progristillidan@gmail.com>
2022-11-03 18:39:34 +0300

    выполнила кучу просьб заказчика + добавила метод играть

--- Main.kt
+++ Main.kt
@@ -7,8 +7,10 @@ fun main() {
         when (action) {
             "sleep" -> cat.sleep()
             "feed" -> cat.feed()
-            "wake up" -> cat.wakeUp()
-            "stop eat" -> cat.stopEat()
+            "play" -> cat.play()
+            // "wake up" -> cat.wakeUp()
+            // "stop eat" -> cat.stopEat()
+            // "stop play" -> cat.stopPlay()
         }
     }
 }
@@ -16,63 +18,124 @@ fun main() {
 class Tamagochi {
     var energy: Int = 10
     var safiety: Int = 10
+    var funny: Int = 10
     var status: String? = null
 
     fun show() {
         println("energy: " + energy)
         println("safiety: " + safiety)
+        println("funny: " + funny)
         println(" ")
     }
 
     fun sleep() {
-        if (status == null) {
+        // if (status == null) {
+       // if (status != "tamagochi is sleeping") {
             when {
                 this.energy < 9 -> this.energy += 2
                 this.energy == 9 -> this.energy++
+                this.energy == 10 -> println("я уже выспался")
             }
-            this.safiety--
+            if (safiety > 0)
+                this.safiety--
+            if (funny > 0)
+                this.funny--
             this.show()
             status = "tamagochi is sleeping"
             println(status)
-        }else if(status == "tamagochi is eating"){
-            println("tamagochi is eating! run stop eat")
-        }else if(status == "tamagochi is sleeping"){
-            println("tamagochi already sleep")
-        }
+      // }else{
+      //     println("tamagochi already sleep")
+      // }
+            // }else if(status == "tamagochi is eating"){
+            //     println("tamagochi is eating! run stop eat")
+            // }else if(status == "tamagochi is playing") {
+            //     println("tamagochi is playing! run stop play")
+            // }else if(status == "tamagochi is sleeping") {
+            //     println("tamagochi already sleep")
+            // }
+        //}
     }
 
     fun feed() {
-        if (status == null) {
+        //if (status == null) {
+       // if (status != "tamagochi is eating") {
             when {
                 this.safiety < 9 -> this.safiety += 2
                 this.safiety == 9 -> this.safiety++
+                this.safiety == 10 -> println("я уже сытый")
             }
-            this.energy--
+            if (funny > 0)
+                this.energy--
+            if (funny < 10)
+                this.funny++
             this.show()
             status = "tamagochi is eating"
             println(status)
-        }else if(status == "tamagochi is sleeping"){
-            println("tamagochi is sleeping! run wake up")
-        }else if(status == "tamagochi is eating") {
-            println("tamagochi already eat")
-        }
+       // }else{
+       //     println("tamagochi already eat")
+       // }
+            // }else if(status == "tamagochi is sleeping"){
+            //     println("tamagochi is sleeping! run wake up")
+            // }else if(status == "tamagochi is playing") {
+            //     println("tamagochi is playing! run stop play")
+            // }else if(status == "tamagochi is eating") {
+            //     println("tamagochi already eat")
+            // }
+        //}
     }
 
-    fun wakeUp() {
-        if (status == "tamagochi is sleeping") {
-            status = null
-            println("tamagochi woke up")
-        }else{
-            println("tamagochi is not sleeping")
-        }
+    fun play() {
+      //  if (status == null) {
+        //if (status != "tamagochi is playing") {
+            when {
+                this.funny < 9 -> this.funny += 2
+                this.funny == 9 -> this.funny++
+                this.funny == 10 -> println("я уже наигрался")
+            }
+            if (energy > 0)
+                this.energy--
+            if (safiety > 0)
+                this.safiety--
+            this.show()
+            status = "tamagochi is playing"
+            println(status)
+       // }else{
+       //     println("tamagochi already play")
+       // }
+            // }else if(status == "tamagochi is sleeping"){
+            //     println("tamagochi is sleeping! run wake up")
+            // }else if(status == "tamagochi is eating") {
+            //     println("tamagochi is eating! run stop eat")
+            // }else if(status == "tamagochi is playing") {
+            //     println("tamagochi already play")
+            // }
+       // }
     }
 
-    fun stopEat() {
-        if (status == "tamagochi is eating") {
-            status = null
-            println("tamagochi stop eat")
-        }else{
-            println("tamagochi is not eating")
-        }
-    }
+    // fun wakeUp() {
+    //     if (status == "tamagochi is sleeping") {
+    //         status = null
+    //         println("tamagochi woke up")
+    //     }else{
+    //         println("tamagochi is not sleeping")
+    //     }
+    // }
+//
+    // fun stopEat() {
+    //     if (status == "tamagochi is eating") {
+    //         status = null
+    //         println("tamagochi stop eat")
+    //     }else{
+    //         println("tamagochi is not eating")
+    //     }
+    // }
+//
+    // fun stopPlay() {
+    //     if (status == "tamagochi is playing") {
+    //         status = null
+    //         println("tamagochi stop play")
+    //     }else{
+    //         println("tamagochi is not playing")
+    //     }
+    // }
 }
