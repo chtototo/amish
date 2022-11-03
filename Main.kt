ea8c4f555c01ad67218940bb93dd4341050d0f78
Amina Magomedova <progristillidan@gmail.com>
2022-11-03 18:05:50 +0300

    добавила статус и 2 метода для того, чтобы разбудить и перестать кушать

--- Main.kt
+++ Main.kt
@@ -7,6 +7,8 @@ fun main() {
         when (action) {
             "sleep" -> cat.sleep()
             "feed" -> cat.feed()
+            "wake up" -> cat.wakeUp()
+            "stop eat" -> cat.stopEat()
         }
     }
 }
@@ -14,6 +16,7 @@ fun main() {
 class Tamagochi {
     var energy: Int = 10
     var safiety: Int = 10
+    var status: String? = null
 
     fun show() {
         println("energy: " + energy)
@@ -22,21 +25,54 @@ class Tamagochi {
     }
 
     fun sleep() {
-        when {
-            this.energy < 9 -> this.energy += 2
-            this.energy == 9 -> this.energy++
+        if (status == null) {
+            when {
+                this.energy < 9 -> this.energy += 2
+                this.energy == 9 -> this.energy++
+            }
+            this.safiety--
+            this.show()
+            status = "tamagochi is sleeping"
+            println(status)
+        }else if(status == "tamagochi is eating"){
+            println("tamagochi is eating! run stop eat")
+        }else if(status == "tamagochi is sleeping"){
+            println("tamagochi already sleep")
         }
-        this.safiety--
-        this.show()
     }
 
     fun feed() {
-        when {
-            this.safiety < 9 -> this.safiety += 2
-            this.safiety == 9 -> this.safiety++
+        if (status == null) {
+            when {
+                this.safiety < 9 -> this.safiety += 2
+                this.safiety == 9 -> this.safiety++
+            }
+            this.energy--
+            this.show()
+            status = "tamagochi is eating"
+            println(status)
+        }else if(status == "tamagochi is sleeping"){
+            println("tamagochi is sleeping! run wake up")
+        }else if(status == "tamagochi is eating") {
+            println("tamagochi already eat")
         }
-        this.energy--
-        this.show()
+    }
+
+    fun wakeUp() {
+        if (status == "tamagochi is sleeping") {
+            status = null
+            println("tamagochi woke up")
+        }else{
+            println("tamagochi is not sleeping")
+        }
+    }
+
+    fun stopEat() {
+        if (status == "tamagochi is eating") {
+            status = null
+            println("tamagochi stop eat")
+        }else{
+            println("tamagochi is not eating")
+        }
     }
 }
-
